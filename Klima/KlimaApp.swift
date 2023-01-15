a20f4c8f9698c9772c658cdd1e44e7d876e40f30
Habibur Rahman <hr_habib@hotmail.com>
2023-01-15 03:25:42 +0100

    Initial Commit

---
+++ Klima/KlimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlimaApp.swift
+//  Klima
+//
+//  Created by Office on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct KlimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
