b2f2679f2959b1ba585b122abe1f889a4621e217
Habibur Rahman <hr_habib@hotmail.com>
2023-01-15 04:34:57 +0100

    Add current weather by city name endpoint

--- Klima/Networking/Endpoint.swift
+++ Klima/Networking/Endpoint.swift
@@ -12,12 +12,15 @@ enum Endpoint {
         static let baseURL: String = "https://api.openweathermap.org/data/2.5"
         static let appId: String = "f1e1e54bd180683469368ad612c7f945"
     }
-    case currentWeather(lat: Double, lon: Double)
+    case currentWeatherByLocation(lat: Double, lon: Double)
+    case currentWeatherByCityName(cityName: String)
     
     var path: String {
         switch self {
-        case .currentWeather(let lat, let lon):
+        case .currentWeatherByLocation(let lat, let lon):
             return "\(Constants.baseURL)/weather?appid=\(Constants.appId)&units=metric&lat=\(lat)&lon=\(lon)"
+        case .currentWeatherByCityName(let cityName):
+            return "\(Constants.baseURL)/weather?appid=\(Constants.appId)&units=metric&q=\(cityName)"
             
         }
     }
--- KlimaTests/EndpointTests.swift
+++ KlimaTests/EndpointTests.swift
@@ -10,8 +10,13 @@ import XCTest
 final class EndpointTests: XCTestCase {
 
     func testCurrentWeatherByLocationEndpointIsValid() {
-        let endpoint = Endpoint.currentWeather(lat: 59.329323, lon:18.068581)
+        let endpoint = Endpoint.currentWeatherByLocation(lat: 59.329323, lon:18.068581)
         XCTAssertEqual(endpoint.url?.absoluteString, "https://api.openweathermap.org/data/2.5/weather?appid=f1e1e54bd180683469368ad612c7f945&units=metric&lat=59.329323&lon=18.068581", "The endpoint is invalid")
+    }
+    
+    func testCurrentWeatherByCityNameEndpointIsValid() {
+        let endpoint = Endpoint.currentWeatherByCityName(cityName: "stockholm")
+        XCTAssertEqual(endpoint.url?.absoluteString, "https://api.openweathermap.org/data/2.5/weather?appid=f1e1e54bd180683469368ad612c7f945&units=metric&q=stockholm", "The endpoint is invalid")
     }
 
 }
